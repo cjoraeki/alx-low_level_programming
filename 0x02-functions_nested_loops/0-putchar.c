a9c70fc9568530cf3ffd55e514eab910c896c780
Chijioke Oraeki <cjoraeki3@gmail.com>
2022-07-05 16:18:12 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints _putchar to stdout
- *
- * Return: 0
+ * main - main block
+ * Description: print _putchar followed on a new line
+ * Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
